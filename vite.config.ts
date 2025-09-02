11c84abb2eeb39891d7a9b5818138d5904e49a72
balaji7761 <s.balaji123467@gmail.com>
2025-09-02 12:45:30 +0530

    Added GitHub Pages deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react-frontend/",  // 👈 important for GitHub Pages
   css: {
     postcss: './postcss.config.js',
   },
